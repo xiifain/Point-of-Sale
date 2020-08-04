d67f2d18a546c410b93df4f6103684a2dad53df4
Eddie <aungthiha.160898@gmail.com>
2020-08-04 13:20:27 +0700

    Initial Commit

---
+++ Point Of Sales/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Point Of Sales
+//
+//  Created by Aung Thiha on 4/8/20.
+//  Copyright © 2020 Aung Thiha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
